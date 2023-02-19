cd39354465a67c29a9ae07c238aeed124e6645c5
trevorwrightdev <trevor.wright.710@gmail.com>
2023-02-19 16:28:38 -0700

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,12 @@
+
+const Game = () => {
+  return (
+    <iframe src='./game/index.html' style={{
+        borderWidth: 0,
+        height: '100%',
+        width: '100%',
+    }}/>
+  )
+}
+
+export default Game
