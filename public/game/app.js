bc77e436a80cb080dc6641741c91874f16980721
trevorwrightdev <trevor.wright.710@gmail.com>
2023-02-19 18:36:51 -0700

    got scale mode to work

--- public/game/app.js
+++ public/game/app.js
@@ -1,14 +1,9 @@
 PIXI.settings.RESOLUTION = 5
 
+PIXI.BaseTexture.defaultOptions.scaleMode = PIXI.SCALE_MODES.NEAREST
+
 const width = 640
 const height = 480
-
-// var sound = new Howl({
-//     src: ['./sounds/crickets.mp3'],
-//     loop: true,
-// });
-
-// sound.play()
 
 const app = new PIXI.Application({
     width: width,
@@ -22,13 +17,52 @@ async function loadAllAssets() {
     assets = await PIXI.Assets.loadBundle("assets")
 }
 
-// this function runs on start.
-async function startGame() {
-    
-    // load all of our assets before we do anything 
-    await loadAllAssets()
+function play() {
+    // remove all children from body
+    document.body.innerHTML = ''
+    // add stage to body
     document.body.appendChild(app.view)
+
+    // play bg ambience
+    let sound = new Howl({
+        src: ['./sounds/crickets.mp3'],
+        loop: true,
+    });
+    sound.play()
+    loadBedroom()
 }
 
-startGame()
+function loadBedroom() {
+    // load background
+    const background = new PIXI.Sprite(assets['bedroom'])
+    background.width = width
+    background.height = height
+    app.stage.addChild(background)
+}
+
+function loadMainMenu() {
+    const playButton = document.createElement('button')
+    playButton.innerText = 'PLAY'
+
+    playButton.style.border = 'none'
+    playButton.style.background = 'none'
+    playButton.style.color = 'white'
+    playButton.style.fontSize = '50px'
+    playButton.style.fontFamily = 'VT323'
+    playButton.style.cursor = 'pointer'
+
+    playButton.addEventListener('click', play)
+
+    document.body.appendChild(playButton)
+}
+
+// this function runs on page load
+async function setUpGame() {
+    // load all of our assets before we do anything 
+    await loadAllAssets()
+    // set up our main menu
+    loadMainMenu()
+}
+
+setUpGame()
 
